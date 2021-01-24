f01138955438f01445041b82596922ae189a9a8c
Franck Nijhof <git@frenck.dev>
2021-01-24 18:42:57 +0100

    Upgrade add-on base image to 9.1.0

--- appdaemon/rootfs/etc/cont-init.d/appdaemon.sh
+++ appdaemon/rootfs/etc/cont-init.d/appdaemon.sh
@@ -28,7 +28,7 @@ if bashio::config.has_value 'python_packages'; then
     for package in $(bashio::config 'python_packages'); do
         pip3 install \
             --prefer-binary \
-            --find-links "https://wheels.home-assistant.io/alpine-3.12/${arch}/" \
+            --find-links "https://wheels.home-assistant.io/alpine-3.13/${arch}/" \
             "$package" \
                 || bashio::exit.nok "Failed installing package ${package}"
     done

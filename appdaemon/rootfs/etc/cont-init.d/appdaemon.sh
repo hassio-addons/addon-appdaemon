54b7e770bd8e65703ee9c7fbdfb46d6b39cc60ec
Franck Nijhof <frenck@addons.community>
2021-02-02 17:13:18 +0100

    Raise fatal error message on missing AppDaemon configuration file (#74)
    * Raise fatal error message on missing AppDaemon configuration file
    
    * Simper...

--- appdaemon/rootfs/etc/cont-init.d/appdaemon.sh
+++ appdaemon/rootfs/etc/cont-init.d/appdaemon.sh
@@ -11,6 +11,20 @@ if ! bashio::fs.directory_exists '/config/appdaemon'; then
         || bashio::exit.nok 'Failed to create initial AppDaemon configuration'
 fi
 
+# Raise warning if the directory exists, but the appdaemon config is missing.
+if ! bashio::fs.file_exists '/config/appdaemon/appdaemon.yaml'; then
+    bashio::log.fatal
+    bashio::log.fatal "Seems like the /config/appdaemon folder exists,"
+    bashio::log.fatal "however appdaemon.yaml wasn't found."
+    bashio::log.fatal
+    bashio::log.fatal "Remove or rename the /config/appdaemon folder"
+    bashio::log.fatal "and the add-on will create a new and fresh one"
+    bashio::log.fatal "for you."
+    bashio::log.fatal
+
+    bashio::exit.nok
+fi
+
 # Install user configured/requested packages
 if bashio::config.has_value 'system_packages'; then
     apk update \
